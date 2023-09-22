cc8f26126929fd4ad338146f8e7b715f3d40dd9a
contadorlucassales <srlucassales@gmail.com>
2023-09-22 19:56:10 -0300

    23-09-22

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Site | GN</title>
+  <style>
+  body {}
+  </style>
+</head>
+
+<body>
+  <h1>Se inscreva no canal ;)</h1>
+  <h2>Lucas Sales</h2>
+  <div class="box">
+    <a href="">Login</a>
+    <a href="">Cadastre-se</a>
+  </div>
+
+</body>
+
+</html>
