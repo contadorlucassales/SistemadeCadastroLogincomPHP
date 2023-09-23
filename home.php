19893f9da425ca1a7a1694d217eb9009180fcedb
contadorlucassales <srlucassales@gmail.com>
2023-09-22 22:59:41 -0300

    23-09-22

--- home.php
+++ home.php
@@ -5,8 +5,36 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Site | GN</title>
+
   <style>
-  body {}
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+      background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+      text-align: center;
+      color: white;
+    }
+
+    .box {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 30px;
+      border-radius: 15px;
+    }
+
+    a {
+      text-decoration: none;
+      color: white;
+      border: 3px solid dodgerblue;
+      padding: 10px;
+      border-radius: 10px;
+    }
+
+    a:hover {
+      background-color: dodgerblue;
+    }
   </style>
 </head>
 
@@ -14,8 +42,8 @@
   <h1>Se inscreva no canal ;)</h1>
   <h2>Lucas Sales</h2>
   <div class="box">
-    <a href="">Login</a>
-    <a href="">Cadastre-se</a>
+    <a href="login.php">Login</a>
+    <a href="formulario.php">Cadastre-se</a>
   </div>
 
 </body>
